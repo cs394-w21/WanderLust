ca59b2638430c1c6a6ce4faaafd90a1c2fde7fd6
Hamilton <hamiltonvuu@gmail.com>
2021-02-12 15:25:30 -0600

    remove filler + add packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  const stuff = render(<App />);
+  console.log(stuff);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { GoogleMap, Marker } from "react-google-maps";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const Map = (props) => (
+  <>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <Typography>Hello world</Typography>
+  </>
+);
+
+export default Map;
