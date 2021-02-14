897cbedc50d5e2ff19352ee9cd4e5ee4e406e28e
Alex Chao <achao2018@gmail.com>
2021-02-14 11:12:20 -0600

    fixing octocat

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,8 @@ test("renders the world at your finger tips", () => {
   const stuff = render(<App />);
   expect(screen.getByText(/The World at your Finger Tips/i)).toBeInTheDocument();
 });
+
+test("renders create new trip", () => {
+  const stuff = render(<App />);
+  expect(screen.getByText(/Create New Trip/i)).toBeInTheDocument();
+});
