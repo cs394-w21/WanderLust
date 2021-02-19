57ec93ee296c3ef38a31fa346162255b74729ff5
hamiltonvuu <hamiltonvuu@gmail.com>
2021-02-19 13:45:50 -0600

    pins now holds username and user profile picture

--- src/utils/useFirebaseLocations.js
+++ src/utils/useFirebaseLocations.js
@@ -1,11 +1,11 @@
 import React from "react";
 import firebase from "./firebase";
 
-const toRenderableArray = (obj) =>
-  Object.entries(obj).map(([key, val]) => ({
-    id: key,
-    ...val,
-  }));
+const joinAccountToPins = (accounts, pins) =>
+  Object.entries(pins).map(([key, pin]) => {
+    const account = accounts[pin.user];
+    return { id: key, userName: account.name, userPic: account.img, ...pin };
+  });
 
 const useFirebaseLocations = (userId) => {
   // NOTE: If we change the structure of our database later,
@@ -13,12 +13,14 @@ const useFirebaseLocations = (userId) => {
   const [locations, setLocations] = React.useState(null);
   const handleNewLocations = React.useCallback(
     (snap) => {
-      setLocations(toRenderableArray(snap.val()));
+      const pins = joinAccountToPins(snap.val().accounts, snap.val().pins);
+      console.log(pins);
+      setLocations(pins);
     },
     [setLocations]
   );
   React.useEffect(() => {
-    const db = firebase.database().ref("users").child(userId);
+    const db = firebase.database().ref();
     db.on("value", handleNewLocations, window.alert);
     return () => {
       db.off("value", handleNewLocations);
