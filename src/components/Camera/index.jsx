7a5b4c2e257c2b2fe06663519ae6fcc118ab320a
Alex Chao <achao2018@gmail.com>
2021-02-27 22:08:45 -0600

    add photo & location

--- src/components/Camera/index.jsx
+++ src/components/Camera/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import Fade from "@material-ui/core/Fade";
 import Typography from "@material-ui/core/Typography";
@@ -7,11 +7,69 @@ import { useWindowWidth } from "@react-hook/window-size";
 import useNavbar from "../../utils/useNavbarContext.js";
 import Flex from '../../components/Flex';
 import { useModalStyles } from "../../utils/popupStyles";
+import ImageUploader from 'react-images-upload';
+import { GoogleMap, Autocomplete, useGoogleMap } from "@react-google-maps/api";
+ 
 
 const getPaperWidth = (width) => {
   if (width > 500) return 400;
   if (width > 350) return width / 2;
   return width - 90;
+};
+
+
+
+const Picture = () => {
+  const [picture, setPicture] = useState(null);
+  
+  console.log(picture);
+
+  /*
+    1. Choose Picture
+    2. Add location search
+    3. Add Description/Activity Type
+    4. Make it a form
+    5. Use that data to add to map
+  */
+
+  return (
+    <ImageUploader 
+      withIcon={true}
+      buttonText='Choose image'
+      onChange={setPicture}
+      withPreview={true}
+      label='Max File Size: 5mb, Accepted: jpg, png'
+      imgExtension={['.jpg', '.gif', '.png', '.gif']}
+      maxFileSize={5242880}
+    />
+  )
+}
+
+const LocationSearch = () => {
+  const [autocomplete, setAutocomplete] = React.useState(3);
+  const map = useGoogleMap();
+  if (!autocomplete) return null;
+  return (
+    <Autocomplete
+      onLoad={setAutocomplete}
+    >
+      <input
+        type="text"
+        placeholder="Search for Location"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`,
+        }}
+      />
+    </Autocomplete>
+  );
 };
 
 const CameraBody = () => {
@@ -31,6 +89,10 @@ const CameraBody = () => {
             My Photos
           </Typography>
           <CancelIcon style={{ cursor: "pointer" }} onClick={closePopup} />
+        </Flex>
+        <Picture />
+        <Flex justifyContent="center"> {/*make this a form*/} 
+          <LocationSearch/>
         </Flex>
       </Flex>
     </Fade>
