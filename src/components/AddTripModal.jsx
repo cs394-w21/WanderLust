2871248e02cd5a7ec83948ab26742d9eebba6242
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-03-05 10:08:00 -0600

    Remove warnings

--- src/components/AddTripModal.jsx
+++ src/components/AddTripModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useFormikContext, useField, Formik, Form } from "formik";
+import { useField, Formik, Form } from "formik";
 import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
