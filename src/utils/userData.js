91eb4aa8db892769b1efe7f139192c34fd52be92
hamiltonvuu <hamiltonvuu@gmail.com>
2021-03-08 13:52:31 -0600

    exif data + clearing pins + default user

---
+++ src/utils/userData.js
@@ -0,0 +1,6 @@
+const userUUID = "8771fefd-268a-4a4d-b1af-b2961f2beefc";
+const userName = "Clark";
+const userPic =
+  "https://firebasestorage.googleapis.com/v0/b/wanderlust-708c7.appspot.com/o/clarkg.jpg?alt=media&token=c8ec8e80-6a09-41ef-b0c4-e96b4a368d24";
+
+export { userUUID, userName, userPic };
